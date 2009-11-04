6cc8b93867fb861825dc36e77975bd5a430730a3
Acyuta <acyuta108@gmail.com>
2009-11-03 19:42:10 -0500

    Added minor config attribute to autocomplete for cat and page search suggest

--- search_suggest.js
+++ search_suggest.js
@@ -1,3 +1,4 @@
+
 /***********************************************************
  *      This script is a search suggest plug-in
  *      for Vaniquotes that will give Pages and Category
@@ -5,27 +6,22 @@
  ************************************************************/
 
 $(document).ready(function() {
-    var options_search = {
+    $('#searchInput').autocomplete("/php/cat-page_search.php", {
         minChars: 3,
         resultsClass: 'cat-page-suggest_results',
         max: 100,
         delay: 500,
-        width: '45em',
         caheLength: 1,
         fixed: false,
+        width: '45em',
         matchContains: true,
+        selectFirst: false,
         extraParams: { namespace: 'all' }
-    }
-    $('#searchInput').autocomplete("/php/cat-page_search.php", options_search);
-    // Test if we are in Special:Search
-    if($('form#search #searchText').length === 1) {
-        options_search['width'] = false;
-        $('form#search #searchText').autocomplete("/php/cat-page_search.php", options_search);
-    }
+    });
     if(wgPageName === "Special:AllPages") {
         var table = $('table.allpages');
         var ns = $('#namespace option:selected', table).val() //element selected for searching
-        var options_allpages = {
+        var options = {
             minChars: 3,
             resultsClass: 'cat-page-suggest_results',
             max: 45,
@@ -33,11 +29,12 @@ $(document).ready(function() {
             caheLength: 1,
             fixed: false,
             matchContains: true,
+            selectFirst: false,
             extraParams: { namespace: ns  }
         };
         if(ns === '0') {
-            options_allpages['width'] = '45em';
+            options['width'] = '45em';
         }
-        $('#nsfrom', table).autocomplete("/php/cat-page_search.php", options_allpages);
+        $('#nsfrom', table).autocomplete("/php/cat-page_search.php", options);
    }
 });
