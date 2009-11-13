e7bf0cb8ef390671643a8a6cedf4dd802ca60cdf
Acyuta <acyuta108@gmail.com>
2009-11-12 22:31:30 -0500

    Update

--- convert_devanagari.js
+++ convert_devanagari.js
@@ -11,18 +11,9 @@ $(document).ready(function() {
     // can correct spacing and other stuff that may come up
     $('.verse, .converted').click(function () {
         $(this).editable(function(text) {
-            text = text.replace(/ \)\)( |$)/g, ' ॥ ');
-            text = text.replace(/ \)( |$)/g, ' । ');
-            text = text.replace(/0/g, '०');
-            text = text.replace(/1/g, '१');
-            text = text.replace(/2/g, '२');
-            text = text.replace(/3/g, '३');
-            text = text.replace(/4/g, '४');
-            text = text.replace(/5/g, '५');
-            text = text.replace(/6/g, '६');
-            text = text.replace(/7/g, '७');
-            text = text.replace(/8/g, '८');
-            text = text.replace(/9/g, '९');
+            if(text.match(/\)$/m)) {
+                text = fix_numbers(text);
+            }
             return text;
         },{
             type: 'textarea',
@@ -31,13 +22,48 @@ $(document).ready(function() {
     });
     // Grab the submit action and do the ajax submit
     $('.submit').click(function() {
+        $('#ajax > #msg').text('Fetching devanagari...')
         verse = clean_for_submit($(this).siblings('.verse').html());
         translate_diCrunch(verse, this);
     });
-    $('<div id="loading">Retrieving Devanagari...<br/><img src="/css/indicator.gif"></img></div>').
-    ajaxStart(function() { $(this).show(); }).
-    ajaxStop(function() { $(this).fadeOut(); }).
-    appendTo('body');
+    $('.fix_numbers').click(function() {
+        message('Fixing numbers...')
+        orig = $(this).siblings('.indevr');
+        deva = $(this).siblings('.converted');
+        orig_a = orig.html().split(/<br\/?>/);
+        deva_a = deva.html().split(/\n/);
+        if(orig_a.length === deva_a.length) {
+            $.each(orig_a, function(i, v) {
+                if(v.match(/\)+$/m)) {
+                    numbers = v.match(/( (?:\)\) \d+ \))*\))/)[1];
+                    numbers = fix_numbers(numbers);
+                    deva_a[i] += numbers;
+                }
+            });
+            new_deva = deva_a.join('\n');
+            if(deva.html() !== new_deva) {
+                deva.html(new_deva);
+                console.log(new_deva);
+            }
+        } else {
+            alert("Original devanagari is not equal to converted!");
+        }
+    });
+    // Create loading div for ajax and other messages. Make sure you place the message before the ajax call
+    $('<div id="ajax"><span id="msg"></span><br/><img src="/css/indicator.gif"></img></div>').
+        ajaxStart(function() { $(this).show(); }).
+        ajaxStop(function() { $(this).fadeOut('slow'); }).
+        appendTo('body');
+    // Save
+    $('.save').click(function() {
+            $('#ajax > #msg').text('Saving...');
+        $.get('/php/convert_devanagari_save.php', {
+            id: $(this).siblings('.id').text(),
+            deva: $(this).siblings('.converted').text()
+        }, function(r) {
+            message('Saved!');
+        });
+    })
 });
 
 
@@ -58,4 +84,23 @@ function clean_for_submit(v) {
     v = v.replace(/-/g, '');
     v = v.replace(/ '/g, '');
     return v;
+}
+function fix_numbers(n) {
+    n = n.replace(/ \)\)( |$)/mg, ' ॥ ');
+    n = n.replace(/ \)( |$)/mg, ' । ');
+    n = n.replace(/0/g, '०');
+    n = n.replace(/1/g, '१');
+    n = n.replace(/2/g, '२');
+    n = n.replace(/3/g, '३');
+    n = n.replace(/4/g, '४');
+    n = n.replace(/5/g, '५');
+    n = n.replace(/6/g, '६');
+    n = n.replace(/7/g, '७');
+    n = n.replace(/8/g, '८');
+    n = n.replace(/9/g, '९');
+    return n;
+}
+function message(msg) {
+    $('#ajax > #msg').text(msg).parent().show();
+    setTimeout(function() { $('#ajax').fadeOut('slow') }, 1000);
 }
