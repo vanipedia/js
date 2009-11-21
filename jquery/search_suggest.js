1987b6c36e7ac69e9d4cd1bce452417dd58a17e0
Acyuta <acyuta108@gmail.com>
2009-11-21 16:44:49 -0500

    Add search_suggest back to vanisource

--- jquery/search_suggest.js
+++ jquery/search_suggest.js
@@ -3,16 +3,16 @@
  *  It uses jquery.autocomplete and all its dependencies to fetch suggestions from a php script in the server
  *  and show it in a suggest box as the visitor types.
  ***********************************************************************************************************/
-if ($('#searchInput') || $('#google_search')) {
-	$(document).ready(function () {
-		$("#searchInput").autocomplete("/php_scripts/dictionary.php", {
-    scroll: true,
-		  autofill: false,
-		  selectFirst: false,
-		  maxItemsToShow: 10,
-		  minChars: 2,
-		  multiple: true,
-		  multipleSeparator: ' '
-		});
-	});
+if ($('#searchInput')) {
+    $(document).ready(function () {
+        $("#searchInput").autocomplete("/php_scripts/dictionary.php", {
+            scroll: true,
+            autofill: false,
+            selectFirst: false,
+            maxItemsToShow: 10,
+            minChars: 2,
+            multiple: true,
+            multipleSeparator: ' '
+        });
+    });
 }
