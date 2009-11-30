0fd545d9dba1bd39f5125ea6f0eeebbe570995e2
Acyuta <acyuta108@gmail.com>
2009-11-30 13:48:55 -0500

    Minor changes for diff page

--- gita_compare.js
+++ gita_compare.js
@@ -1,7 +1,10 @@
 
-if(/&diff=/.test(document.location)) {
+if(/^BG \d+\.\d+/.test(wgTitle) && /&diff=/.test(document.location)) {
     $(document).ready(function() {
         $('#diff_link').parent().hide();
+        if(!/&htmldiff=0/.test(document.location)) {
+            $('.diff').hide();
+        }
     });
 }
 
