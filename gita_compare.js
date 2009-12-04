dd3ee4469fc6ff6662f6379b2e8fc27c7700618c
Acyuta <acyuta108@gmail.com>
2009-12-03 20:36:17 -0500

    In diff element .verse_tools should be hidden otherwise we get double random_pic and navigation

--- gita_compare.js
+++ gita_compare.js
@@ -1,10 +1,9 @@
 
 if(/^BG \d+\.\d+/.test(wgTitle) && /&diff=/.test(document.location)) {
     $(document).ready(function() {
-        $('#diff_link').parent().hide();
+        $('.verse_tools').hide();
         if(!/&htmldiff=0/.test(document.location)) {
             $('.diff').hide();
         }
     });
 }
-
