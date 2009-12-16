0b9e10158a52604724bcbad59654469aae46daf6
Acyuta <acyuta108@gmail.com>
2009-12-16 18:02:57 -0500

    Merge changes from develoment

--- gita_compare.js
+++ gita_compare.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-    if(/^BG /.test(wgTitle)) {
-        $("#text").css({ 'white-space': 'pre' });
-        if(/&diff=/.test(document.location)) {
-            $('.verse_tools').hide();
-            if(!/&htmldiff=0/.test(document.location)) {
-                $('.diff').hide();
-            }
+    if(wgTitle == 'BG edition comparison') {
+        $('.verse_tools').hide();
+        if(!/&visual=0/.test(document.location)) {
+            $("#devanagari, #text").css('white-space', 'normal');
         }
     }
 });
