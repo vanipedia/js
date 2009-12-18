b5141dcf8dc0470f9faba52cd29360ec1f489177
Acyuta <acyuta108@gmail.com>
2009-12-17 22:53:07 -0500

    Decoupling compare script to be usable with any text compare

--- text_compare.js
+++ text_compare.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    if(wgTitle == 'BG edition comparison') {
+    if(wgTitle == 'Edition comparison') {
         $('.verse_tools').hide();
         if(!/&visual=0/.test(document.location)) {
             $("#devanagari, #text").css('white-space', 'normal');
