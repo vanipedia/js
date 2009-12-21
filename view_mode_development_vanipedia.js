4ea19129023d2f75017f40b41fa545f5855e1d96
Acyuta <acyuta108@gmail.com>
2009-12-21 18:17:55 -0500

    Created production and development scripts for vanipedia. Display of statistics requires the stats-submit and jQuery

---
+++ view_mode_development_vanipedia.js
@@ -0,0 +1,8 @@
+// We'll use the files that already exist in the compile app: no need to double and we can keep those in versioning system as well.
+var resources = '/js/compile-2.0/resources/';
+var jquery = '/js/jquery/';
+var css = '/css/';
+var js = '/js/';
+
+document.write('<script type="text/javascript" src="'+resources+'jquery.min.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'stats_submit.js"></' + 'script>');
