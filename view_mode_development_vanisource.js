9f18aee42408ac889b8ac2604566c86574f91717
Acyuta <acyuta108@gmail.com>
2009-12-20 19:04:11 -0500

    Repository recovery after deletion of files and reorganizing of development and production dependencies and their scripts

---
+++ view_mode_development_vanisource.js
@@ -0,0 +1,24 @@
+var jquery_dir = '/js/jquery/';
+
+// install jQuery
+document.write('<script type="text/javascript" src="'+jquery_dir +'jquery.min.js"></' + 'script>');
+// install UniToASCII
+document.write('<script type="text/javascript" src="/js/UnitoASCII.js"></' + 'script>');
+// install Regexp.escape
+document.write('<script type="text/javascript" src="/js/RegExp.escape.js"></' + 'script>');
+// install HighlightTermsURL
+document.write('<script type="text/javascript" src="/js/highlight_terms_url2.js"></' + 'script>');
+// install ScrollTo
+document.write('<script type="text/javascript" src="'+ jquery_dir +'scrollTo.js"></' + 'script>');
+// install autocomplete jQuery plugin
+document.write('<script type="text/javascript" src="'+jquery_dir +'jquery-autocomplete/jquery.autocomplete.min.js"></' + 'script>');
+// install Vani_suggest
+document.write('<script type="text/javascript" src="/js/sanskrit_dictionary_suggest.js"></' + 'script>');
+// Cookies jQuery plugin
+document.write('<script type="text/javascript" src="'+jquery_dir +'jquery.cookie.js"></' + 'script>');
+// Font detector jQuery plugin
+document.write('<script type="text/javascript" src="/js/fontdetect.js"></' + 'script>');
+// Indevr font detector
+document.write('<script type="text/javascript" src="/js/devanagari_font_detector.js"></' + 'script>');
+// text compare for edition comparison
+document.write('<script type="text/javascript" src="/js/text_compare.js"></' + 'script>');
