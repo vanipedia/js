1b061109c69be205c2d25d8a617ad2248ecbc9af
Acyutananda Caitanya Das <acyuta108@gmail.com>
2011-07-23 06:56:56 +0200

    Add development scripts to production repo for debuggin on server

---
+++ search_suggest.js
@@ -0,0 +1,39 @@
+/***********************************************************
+ *      This script is a search suggest plug-in
+ *      for Vaniquotes that will give Pages and Category
+ *      suggestions to users
+ ************************************************************/
+
+$(document).ready(function() {
+    $('#searchInput').autocomplete("/php/cat-page_search.php", {
+        minChars: 3,
+        resultsClass: 'cat-page-suggest_results',
+        max: 100,
+        delay: 500,
+        caheLength: 1,
+        fixed: false,
+        width: '45em',
+        matchContains: true,
+        selectFirst: false,
+        extraParams: { namespace: 'all' }
+    });
+    if(/Special:/.test(wgPageName)) {
+        var table = $('table.allpages');
+        var ns = $('#namespace option:selected', table).val() //element selected for searching
+        var options = {
+            minChars: 3,
+            resultsClass: 'cat-page-suggest_results',
+            max: 45,
+            delay: 500,
+            caheLength: 1,
+            fixed: false,
+            matchContains: true,
+            selectFirst: false,
+            extraParams: { namespace: ns  }
+        };
+        if(ns === '0') {
+            options['width'] = '45em';
+        }
+        $('#nsfrom', table).autocomplete("/php/cat-page_search.php", options);
+   }
+});
---
+++ view_mode_development_vaniquotes.js
@@ -0,0 +1,24 @@
+// We'll use the files that already exist in the compile app: no need to double and we can keep those in versioning system as well.
+var resources = '/js/compile-2.0/resources/';
+var jquery = '/js/jquery/';
+var css = '/css/';
+var js = '/js/';
+
+document.write('<script type="text/javascript" src="'+resources+'jquery.min.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+resources+'BaltoUni.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+resources+'UniToASCII.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+resources+'jquery.highlight_sanskrit.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+resources+'highlight_terms.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'highlight_terms_url2.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'update_links2.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'RegExp.escape.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'stats_submit.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'search_suggest.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'manual.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+js+'fix_create_link.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+resources+'jquery-autocomplete/jquery.autocomplete.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+jquery+'jdMenu/jquery.bgiframe.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+jquery+'jdMenu/jquery.dimensions.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+jquery+'jdMenu/jquery.positionBy.js"></' + 'script>');
+document.write('<script type="text/javascript" src="'+jquery+'jdMenu/jquery.jdMenu.js"></' + 'script>');
+
